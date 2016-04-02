970a726aa2f05b64032ce405bf15798663f1774f
jasmo2 <jasmo2.com@gmail.com>
2016-04-02 15:19:44 -0500

    refactor iconMe

--- SpoonBox/Views/iconeMe.swift
+++ SpoonBox/Views/iconeMe.swift
@@ -11,19 +11,31 @@ import UIKit
 import GoogleMaps
 
 class IconMe {
+    let iconMeImage: UIImageView
+    let mapView: GMSMapView
+    let myView: UIView
+    
     init(mapView: GMSMapView, view: UIView)  {
         let image = UIImage(named: "icon_me")
-        let iconMeImage = UIImageView(image: image!)
+        iconMeImage = UIImageView(image: image!)
+        self.mapView = mapView
+        myView = view
+    }
+    
+    func centerIcon() {
         iconMeImage.frame = CGRect(x: 0, y: 0, width: 18 , height: 33)
-        
         mapView.addSubview(iconMeImage)
         iconMeImage.translatesAutoresizingMaskIntoConstraints = false;
+        addConstrains(iconMeImage,myView: myView)
+    }
+    
+    private func addConstrains (iconMeImage: UIImageView, myView : UIView){
+        let horizontalConstraint = NSLayoutConstraint(item: iconMeImage, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: myView, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+        myView.addConstraint(horizontalConstraint)
         
-        let horizontalConstraint = NSLayoutConstraint(item: iconMeImage, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
-        view.addConstraint(horizontalConstraint)
-        
-        let verticalConstraint = NSLayoutConstraint(item: iconMeImage, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterY, multiplier: 0.88, constant: 0)
-        view.addConstraint(verticalConstraint)
+        let verticalConstraint = NSLayoutConstraint(item: iconMeImage, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: myView, attribute: NSLayoutAttribute.CenterY, multiplier: 0.92, constant: 0)
+        myView.addConstraint(verticalConstraint)
+    
     }
     
 }
